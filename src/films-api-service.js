2a1293412944acbdf58df8bd2ef62460e18bb497
pt1c <ravenzone@mail.ru>
2022-06-21 23:21:53 +0300

    реализует адаптер

--- src/films-api-service.js
+++ src/films-api-service.js
@@ -15,12 +15,56 @@ export default class FilmsApiService extends ApiService {
     const response = await this._load({
       url: `movies/${film.id}`,
       method: Method.PUT,
-      body: JSON.stringify(film),
+      body: JSON.stringify(this.#adaptToServer(film)),
       headers: new Headers({ 'Content-Type': 'application/json' }),
     });
 
     const parsedResponse = await ApiService.parseResponse(response);
 
     return parsedResponse;
+  };
+
+  #adaptToServer = (film) => {
+    const filmInfo = film.filmInfo;
+    const userDetails = film.userDetails;
+
+    const adaptedFilmInfo = {
+      ...filmInfo,
+      'alternative_title': filmInfo.alternativeTitle,
+      'total_rating': filmInfo.totalRating,
+      'age_rating': filmInfo.ageRating,
+
+      release: {
+        ...filmInfo.release,
+        'release_country': filmInfo.release.releaseCountry
+      }
+    };
+
+    delete adaptedFilmInfo.alternativeTitle;
+    delete adaptedFilmInfo.totalRating;
+    delete adaptedFilmInfo.ageRating;
+    delete adaptedFilmInfo.release.releaseCountry;
+
+    const adaptedUserDetails = {
+      ...userDetails,
+      'already_watched': userDetails.alreadyWatched,
+      'watching_date': userDetails.watchingDate,
+    };
+
+    delete adaptedUserDetails.alreadyWatched;
+    delete adaptedUserDetails.watchingDate;
+
+    const adaptedFilm = {
+      ...film,
+      comments: film.commentsIds,
+      'film_info': adaptedFilmInfo,
+      'user_details': adaptedUserDetails,
+    };
+
+    delete adaptedFilm.commentsIds;
+    delete adaptedFilm.filmInfo;
+    delete adaptedFilm.userDetails;
+
+    return adaptedFilm;
   };
 }
--- src/model/film-model.js
+++ src/model/film-model.js
@@ -12,11 +12,61 @@ export default class FilmModel extends Observable {
     this.#filmsApiService = filmsApiService;
 
     this.#filmsApiService.films.then((films) => {
-      console.log(films);
+      console.log(films.map(this.#adaptToClient));
       // Есть проблема: cтруктура объекта похожа, но некоторые ключи называются иначе,
       // а ещё на сервере используется snake_case, а у нас camelCase.
       // Можно, конечно, переписать часть нашего клиентского приложения, но зачем?
       // Есть вариант получше - паттерн "Адаптер"
+
+      //   {
+      //     "id": "0",
+      //     "film_info": {
+      //         "title": "A Tale Of A Little Bird On The Floor",
+      //         "alternative_title": "Happiness Who Stole The Darkness",
+      //         "total_rating": 9.2,
+      //         "poster": "images/posters/the-great-flamarion.jpg",
+      //         "age_rating": 21,
+      //         "director": "Quentin Tarantino",
+      //         "writers": [
+      //             "Robert Rodrigues",
+      //             "Quentin Tarantino",
+      //             "Stephen King"
+      //         ],
+      //         "actors": [
+      //             "Ralph Fiennes",
+      //             "Edward Norton",
+      //             "Harrison Ford",
+      //             "Al Pacino",
+      //             "Brad Pitt",
+      //             "Gary Oldman",
+      //             "Takeshi Kitano"
+      //         ],
+      //         "release": {
+      //             "date": "2023-04-06T06:50:40.171Z",
+      //             "release_country": "Finland"
+      //         },
+      //         "runtime": 115,
+      //         "genre": [
+      //             "Drama",
+      //             "Horror",
+      //             "Sci-Fi"
+      //         ],
+      //         "description": "Oscar-winning film, a war drama about two young people, true masterpiece where love and death are closer to heroes than their family, a film about a journey that heroes are about to make in finding themselves."
+      //     },
+      //     "user_details": {
+      //         "watchlist": false,
+      //         "already_watched": true,
+      //         "watching_date": "2022-06-20T20:07:20.612Z",
+      //         "favorite": false
+      //     },
+      //     "comments": [
+      //         "1247655",
+      //         "1247656",
+      //         "1247657",
+      //         "1247658",
+      //         "1247659"
+      //     ]
+      // }
     });
   }
 
@@ -51,5 +101,49 @@ export default class FilmModel extends Observable {
     ];
 
     this._notify(updateType, update);
+  };
+
+  #adaptToClient = (film) => {
+    const filmInfo = film.film_info;
+    const userDetails = film.user_details;
+
+    const adaptedFilmInfo = {
+      ...filmInfo,
+      alternativeTitle: filmInfo.alternative_title,
+      totalRating: filmInfo.total_rating,
+      ageRating: filmInfo.age_rating,
+
+      release: {
+        ...filmInfo.release,
+        releaseCountry: filmInfo.release.release_country
+      }
+    };
+
+    delete adaptedFilmInfo.alternative_title;
+    delete adaptedFilmInfo.total_rating;
+    delete adaptedFilmInfo.age_rating;
+    delete adaptedFilmInfo.release.release_country;
+
+    const adaptedUserDetails = {
+      ...userDetails,
+      alreadyWatched: userDetails.already_watched,
+      watchingDate: userDetails.watching_date,
+    };
+
+    delete adaptedUserDetails.already_watched;
+    delete adaptedUserDetails.watching_date;
+
+    const adaptedFilm = {
+      ...film,
+      commentsIds: film.comments,
+      filmInfo: adaptedFilmInfo,
+      userDetails: adaptedUserDetails,
+    };
+
+    delete adaptedFilm.comments;
+    delete adaptedFilm.film_info;
+    delete adaptedFilm.user_details;
+
+    return adaptedFilm;
   };
 }
