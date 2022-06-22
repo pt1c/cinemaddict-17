9c819781fdae97f32b7cda8c6c42e420a6747587
pt1c <ravenzone@mail.ru>
2022-06-22 09:01:37 +0300

    update Film

--- src/model/film-model.js
+++ src/model/film-model.js
@@ -38,20 +38,40 @@ export default class FilmModel extends Observable {
     });
   }
 
-  updateFilm = (updateType, update) => {
+  updateFilm = async (updateType, update) => {
+    // const index = this.#films.findIndex((film) => film.id === update.id);
+
+    // if (index === -1) {
+    //   throw new Error('Can\'t update unexisting film');
+    // }
+
+    // this.#films = [
+    //   ...this.#films.slice(0, index),
+    //   update,
+    //   ...this.#films.slice(index + 1),
+    // ];
+
+    // this._notify(updateType, update);
+
     const index = this.#films.findIndex((film) => film.id === update.id);
 
     if (index === -1) {
       throw new Error('Can\'t update unexisting film');
     }
 
-    this.#films = [
-      ...this.#films.slice(0, index),
-      update,
-      ...this.#films.slice(index + 1),
-    ];
+    try {
+      const response = await this.#filmsApiService.updateFilms(update);
+      const updatedFilm = this.#adaptToClient(response);
+      this.#films = [
+        ...this.#films.slice(0, index),
+        updatedFilm,
+        ...this.#films.slice(index + 1),
+      ];
 
-    this._notify(updateType, update);
+      this._notify(updateType, updatedFilm);
+    } catch (err) {
+      throw new Error('Can\'t update films');
+    }
   };
 
   #adaptToClient = (film) => {
