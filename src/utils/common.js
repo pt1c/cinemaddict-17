a952b957a973b07d6fe86b2429ee439f078e9547
pt1c <ravenzone@mail.ru>
2022-06-23 17:07:03 +0300

    Пренос вызова функций dayJS согласно регламенту

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,11 +1,12 @@
 import dayjs from 'dayjs';
 import duration from 'dayjs/plugin/duration';
-dayjs.extend(duration);
 
 const humanizeRuntime = (runtime) => dayjs.duration(runtime, 'minutes').format('H[h] m[m]').replace(/\b0h\b/, '');
 const humanizeReleaseDate = (date) => dayjs(date).format('D MMMM YYYY');
 const checkMaxStringLength = (inputString, maxLength) => inputString.length <= maxLength;
 const chopString = (inputString, maxLength, etcSymbol) => (checkMaxStringLength(inputString, maxLength)) ? inputString : (inputString.substr(0, maxLength - 1) + etcSymbol);
+
+dayjs.extend(duration);
 
 export {
   humanizeRuntime,
--- src/utils/films.js
+++ src/utils/films.js
@@ -1,12 +1,10 @@
 import dayjs from 'dayjs';
 import duration from 'dayjs/plugin/duration';
 
-dayjs.extend(duration);
-
-
 const sortFilmsByRating = (filmA, filmB) => (filmB.filmInfo.totalRating - filmA.filmInfo.totalRating);
-
 const sortFilmsByDate = (filmA, filmB) => (dayjs(filmB.filmInfo.release.date).diff(dayjs(filmA.filmInfo.release.date)));
+
+dayjs.extend(duration);
 
 export {
   sortFilmsByRating,
